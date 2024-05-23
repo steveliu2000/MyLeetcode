c78eb2114f353c4671877c143432809dd252eac0
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-05-23 18:53:02 +0800

    Create 35_search-insert-position-2.c

---
+++ easy/35_search-insert-position-2.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    // Binary Search 
+    int low = 0;
+    int high = numsSize - 1;
+
+    while(low <= high) {
+        int mid = low + (high-low) / 2;
+        if (target == nums[mid])
+            return mid;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else 
+            high = mid - 1;
+    }
+    return low;
+}
