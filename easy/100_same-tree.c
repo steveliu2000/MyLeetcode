57058c06b0ea97e21dcdc8c33e1ba6abe6d0423c
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-05-24 21:06:41 +0800

    Create 100_same-tree.c

---
+++ easy/100_same-tree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (!p && !q) return true;
+
+    if (p && q) {
+        if (p->val != q->val) return false;
+        if (!isSameTree(p->left, q->left)) return false;
+        if (!isSameTree(p->right, q->right)) return false;
+        return true;
+    }
+    else return false;
+    return true;
+}
