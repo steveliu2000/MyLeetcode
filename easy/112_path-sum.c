f9e4adc859da6dfe574a93ce7a2c94b666358da2
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-05-24 23:55:56 +0800

    Update 112_path-sum.c

--- easy/112_path-sum.c
+++ easy/112_path-sum.c
@@ -10,7 +10,10 @@ bool hasPathSum(struct TreeNode* root, int targetSum) {
     if (!root) return false;
     if (root->val == targetSum && root->right == NULL && root->left == NULL) return true;
     int lack = targetSum - root->val;
+    /*
     if (hasPathSum(root->left, lack)) return true;
     if (hasPathSum(root->right, lack)) return true;
     return false;
+    */
+    return (hasPathSum(root->left, lack) || hasPathSum(root->right, lack));
 }
