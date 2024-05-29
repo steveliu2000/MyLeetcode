c51e7cc24f2ab3d72f937d3428f7db533473ea41
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-05-29 14:44:12 +0800

    Update 7_reverse-integer.c

--- medium/7_reverse-integer.c
+++ medium/7_reverse-integer.c
@@ -1,8 +1,10 @@
 int reverse(int x){
     int output = 0;
+    int max = INT_MAX /10, min = INT_MIN /10;
     while (x) {
         // −2,147,483,648 (−2^31) <= int<= 2,147,483,647 (2^31 − 1)
-        if(output>=214748365||output<=-214748365)
+        // if(output>=214748365||output<=-214748365)
+        if(output > max || output < min)
             return 0;
         output = output*10 + x % 10;
         x /= 10;
