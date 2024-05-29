33b6f87acd85491f9674458ff73164d37d235363
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-05-29 15:14:30 +0800

    Create 8_string-to-integer-atoi.c

---
+++ medium/8_string-to-integer-atoi.c
@@ -0,0 +1,23 @@
+int myAtoi(char* s) {
+    while (*s == ' ')
+        ++s;
+    int neg = 0;
+    if (*s == '-') {
+        neg = 1;
+        ++s;
+    }
+    else if (*s == '+') ++s;
+
+    int ans = 0, max = INT_MAX /10, min = INT_MIN /10;
+    while (isdigit(*s)) {
+        // INT_MAX = 2,147,483,647
+        if(ans > max || (ans == max && (*s-'0')>7)) return INT_MAX;
+        // INT_MIN = −2,147,483,648
+        if(ans < min || (ans == min && (*s-'0')>8)) return INT_MIN;
+
+        if (neg) ans = ans * 10 - (*s-'0');
+        else ans = ans * 10 + (*s-'0');
+        ++s;
+    }
+    return ans;
+}
