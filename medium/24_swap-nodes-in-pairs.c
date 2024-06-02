40ab16a0aad2600312ccf5a7e5c89967d400b2f6
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-06-03 00:09:19 +0800

    Create 24_swap-nodes-in-pairs.c

---
+++ medium/24_swap-nodes-in-pairs.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    if (head == NULL) return NULL;
+    if (head->next == NULL) return head;
+    struct ListNode dummy;
+    struct ListNode* ptr1 = head, * ptr2;
+    if (ptr1 != NULL) ptr2 = ptr1->next;
+    head = &dummy;
+
+    while (ptr1 && ptr2) {
+        ptr1->next = ptr2->next;
+        ptr2->next = ptr1;
+        head->next = ptr2;
+        head = ptr1;
+        ptr1 = ptr1->next;
+        if (ptr1 != NULL) ptr2 = ptr1->next;
+    }
+    return dummy.next;
+}
