e7d243f05d9b121182ec721819472acacb97ba36
steveliu2000 <79685905+steveliu2000@users.noreply.github.com>
2024-06-15 12:55:03 +0800

    Create 40_combination-sum-ii.c

---
+++ medium/40_combination-sum-ii.c
@@ -0,0 +1,39 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int cmp(const void *a,const void *b) {
+    return *((int*) a) - *((int*) b);
+}
+void cSHelper(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes, int layer, int** out) {
+    int rS0;
+    for (int i = 0; i < candidatesSize; ++i) {
+        if (i != 0 && candidates[i] == candidates[i-1])
+            continue;
+        if (target > candidates[i]) {
+            rS0 = *returnSize;
+            cSHelper(&(candidates[i+1]), candidatesSize - i - 1, target - candidates[i], returnSize, returnColumnSizes, layer + 1, out);
+            for (int j = rS0; j < *returnSize; ++j)
+                out[j][layer] = candidates[i];
+        }
+        else if (target == candidates[i]) {
+            out[*returnSize] = malloc((layer + 1) * sizeof(int));
+            out[*returnSize][layer] = target;
+            (*returnColumnSizes)[*returnSize] = layer + 1;
+            ++(*returnSize);
+            break;
+        }
+        else break;
+    }
+}
+
+int** combinationSum2(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes) {
+    qsort(candidates, candidatesSize, sizeof(int), cmp);
+    *returnSize = 0;
+    if (target < candidates[0]) return NULL;
+    int** out = malloc(150 * sizeof(int*));
+    *returnColumnSizes = malloc(150 * sizeof(int));
+    cSHelper(candidates, candidatesSize, target, returnSize, returnColumnSizes, 0, out);
+    return out;
+}
